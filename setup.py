8ac6e9e9e4365b117898912e95805b2ae6140e33
Lyon Brown <lyon.brown@de.abb.com>
2023-05-31 10:50:34 +0200

    added setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+install_requires = [
+    'numpy>=1.17.4',
+    'pandas>=0.25.3',
+    'scikit-learn>=0.22',
+    'tensorflow-probability>=0.8.0',
+    'wget>=3.2',
+    'pyunpack>=0.1.2',
+    'patool>=1.12',
+]
+
+setup(
+    name='tft_self',
+    version='1.0.0',
+    packages=['libs', 'expt_settings', 'data_formatters'],
+    url='https://github.com/adrenalin97/tft_self',
+    license='',
+    author='',
+    author_email='',
+    python_requires='<3.8',
+    description='Self version of TFT google research repo',
+    install_requires=install_requires,
+    include_package_data=True,
+)
